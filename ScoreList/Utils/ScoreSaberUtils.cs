d228f2e820292ddc5d31a507d4bf1fbd757e24f4
Raine Stablum <raine.stablum@gmail.com>
2021-12-05 16:24:12 +0100

    Update ScoreSaberUtils.cs to have classes from yeeted Http.cs

--- ScoreList/Utils/ScoreSaberUtils.cs
+++ ScoreList/Utils/ScoreSaberUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace ScoreList.Utils
@@ -14,6 +15,74 @@ namespace ScoreList.Utils
         {
             public Sprite sprite;
             public string downloadURL;
+        }
+
+        public class ScoreSaberProfile
+        {
+            [JsonProperty("scoreStats")] public ScoreSaberProfileStats ScoreStats;
+        }
+
+        public class ScoreSaberProfileStats
+        {
+            [JsonProperty("totalPlayCount")] public int TotalPlayCount;
+            [JsonProperty("rankedPlayCount")] public int RankedPlayCount;
+        }
+
+        public class ScoreSaberLeaderboardEntry
+        {
+            [JsonProperty("score")] public ScoreSaberLeaderboardScore Score;
+            [JsonProperty("leaderboard")] public ScoreSaberLeaderboardInfo Leaderboard;
+        }
+
+        public class ScoreSaberLeaderboardScore
+        {
+            [JsonProperty("id")] public int Id;
+            [JsonProperty("rank")] public int Rank;
+            [JsonProperty("baseScore")] public int BaseScore;
+            [JsonProperty("modifiedScore")] public int ModifiedScore;
+            [JsonProperty("pp")] public double PP;
+            [JsonProperty("weight")] public double Weight;
+            [JsonProperty("modifiers")] public string Modifiers;
+            [JsonProperty("multiplier")] public double Multiplier;
+            [JsonProperty("badCuts")] public int BadCuts;
+            [JsonProperty("missedNotes")] public int MissedNotes;
+            [JsonProperty("maxCombo")] public int MaxCombo;
+            [JsonProperty("fullCombo")] public bool FullCombo;
+            [JsonProperty("hmd")] public int HMD;
+            [JsonProperty("timeSet")] public string TimeSet;
+            [JsonProperty("hasReplay")] public bool HasReply;
+        }
+
+        public class ScoreSaberLeaderboardInfo
+        {
+            [JsonProperty("id")] public int Id;
+            [JsonProperty("songHash")] public string SongHash;
+            [JsonProperty("songName")] public string SongName;
+            [JsonProperty("songSubName")] public string SongSubName;
+            [JsonProperty("songAuthorName")] public string SongAuthorName;
+            [JsonProperty("levelAuthorName")] public string LevelAuthorName;
+            [JsonProperty("maxScore")] public int MaxScore;
+            [JsonProperty("difficulty")] public ScoreSaberLeaderboardInfoDifficulty Difficulty;
+            [JsonProperty("createdDate")] public string CreatedDate;
+            [JsonProperty("rankedDate")] public string RankedDate;
+            [JsonProperty("qualifiedDate")] public string QualifiedDate;
+            [JsonProperty("lovedDate")] public string LovedDate;
+            [JsonProperty("ranked")] public bool Ranked;
+            [JsonProperty("qualified")] public bool Qualified;
+            [JsonProperty("loved")] public bool Loved;
+            [JsonProperty("maxPP")] public int MaxPP;
+            [JsonProperty("stars")] public double Stars;
+            [JsonProperty("plays")] public int Plays;
+            [JsonProperty("positiveModifiers")] public bool PositiveModifiers;
+            [JsonProperty("playerScore")] public object PlayerScore;
+            [JsonProperty("difficulties")] public object Difficultlies;
+        }
+
+        public class ScoreSaberLeaderboardInfoDifficulty
+        {
+            [JsonProperty("difficulty")] public int Difficulty;
+            [JsonProperty("difficultyRaw")] public string DifficultyRaw;
+            [JsonProperty("gameMode")] public string GameMode;
         }
     }
 }
