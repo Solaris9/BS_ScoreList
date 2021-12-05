5f5176120a09fbb53f0929c792f64473184b956b
Raine Stablum <raine.stablum@gmail.com>
2021-12-05 16:24:52 +0100

    Yeeted references to unneeded code.

--- ScoreList/Plugin.cs
+++ ScoreList/Plugin.cs
@@ -2,14 +2,11 @@
 using IPA;
 using ScoreList.UI;
 using System.IO;
-using System.Security.Policy;
-using System.Threading;
 using IPA.Utilities;
 using IPALogger = IPA.Logging.Logger;
 using ScoreList.Scores;
 using SiraUtil.Zenject;
 using ScoreList.Installers;
-using ScoreList.Utils;
 
 namespace ScoreList
 {
@@ -20,11 +17,10 @@ namespace ScoreList
 
         internal static Plugin Instance { get; private set; }
         internal static IPALogger Log { get; private set; }
-        internal Downloader _downloader;
 
-        public Plugin(Downloader downloader)
+        public Plugin()
         {
-            _downloader = downloader;
+            
         }
 
         [Init]
@@ -35,8 +31,7 @@ namespace ScoreList
             Instance = this;
             Log = logger;
             Log.Info("ScoreList initialized.");
-
-            HttpManger.Init();
+            
             await DatabaseManager.Connect();
         }
 
@@ -46,11 +41,6 @@ namespace ScoreList
             ScoreListUI.instance.Setup();
 
             // DownloadImages();
-        }
-
-        private async void DownloadImages(string url, CancellationToken cancellationToken, Action<float> progressCallback = null)
-        {
-            await _downloader.MakeImageRequestAsync(url, cancellationToken, progressCallback);
         }
     }
 }
