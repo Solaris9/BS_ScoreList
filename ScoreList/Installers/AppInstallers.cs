d1d71ee6e4b5388d38a5b55d31c80c00405df0f2
Raine Stablum <raine.stablum@gmail.com>
2021-12-18 11:08:34 +0100

    Fix NullReferenceException

--- ScoreList/Installers/AppInstallers.cs
+++ ScoreList/Installers/AppInstallers.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.IO;
-using ScoreList.Configuration;
-using ScoreList.Downloaders;
-using ScoreList.Scores;
-using SiraUtil.Logging;
+﻿using IPALogger = IPA.Logging.Logger;
 using Zenject;
 
 namespace ScoreList.Installers
 {
     internal class AppInstaller : Installer
     {
-        private readonly SiraLog _logger;
+        private readonly IPALogger _logger;
         
-        public AppInstaller(SiraLog logger)
+        public AppInstaller(IPALogger logger)
         {
             _logger = logger;
         }
